ae04e9f752c790b29be76379894aad694bedef6d
tkarsakov <115474596+tkarsakov@users.noreply.github.com>
2024-01-19 16:47:27 +0300

    Fix AdminUpdateOption expecting wrong variable

--- src/main/java/com/solvd/construction/ui/util/admin/AdminUpdateOption.java
+++ src/main/java/com/solvd/construction/ui/util/admin/AdminUpdateOption.java
@@ -4,6 +4,7 @@ import com.solvd.construction.model.*;
 import com.solvd.construction.service.factory.ServiceFactory;
 import com.solvd.construction.ui.menuoptions.ObjectSelectOptions;
 import com.solvd.construction.ui.util.input.Input;
+import com.solvd.construction.ui.util.input.helpers.prompters.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -16,14 +17,32 @@ public class AdminUpdateOption implements AdminOption {
         ObjectSelectOptions option = Input.enumInput(ObjectSelectOptions.class);
         LOGGER.info("Enter id");
         Long id = Input.longConsoleInput();
-        Model model = Input.getModelFromConsole(option);
-        model.setId(id);
+        Model model;
         switch (option) {
-            case PROJECTMATERIAL -> serviceFactory.getProjectMaterialService().update((ProjectMaterial) model);
-            case SUPPLIER -> serviceFactory.getSupplierService().update((Supplier) model);
-            case PROJECT -> serviceFactory.getProjectService().update((Project) model);
-            case COUNTRY -> serviceFactory.getCountryService().update((Country) model);
-            case CLIENT -> serviceFactory.getClientService().update((Client) model);
+            case PROJECTMATERIAL:
+                model = Input.getModelFromConsole(new ProjectMaterialPrompter());
+                model.setId(id);
+                serviceFactory.getProjectMaterialService().update((ProjectMaterial) model);
+                return;
+            case SUPPLIER:
+                model = Input.getModelFromConsole(new SupplierPrompter());
+                model.setId(id);
+                serviceFactory.getSupplierService().update((Supplier) model);
+                return;
+            case PROJECT:
+                model = Input.getModelFromConsole(new ProjectPrompter());
+                model.setId(id);
+                serviceFactory.getProjectService().update((Project) model);
+                return;
+            case COUNTRY:
+                model = Input.getModelFromConsole(new CountryPrompter());
+                model.setId(id);
+                serviceFactory.getCountryService().update((Country) model);
+                return;
+            case CLIENT:
+                model = Input.getModelFromConsole(new ClientPrompter());
+                model.setId(id);
+                serviceFactory.getClientService().update((Client) model);
         }
     }
 }
